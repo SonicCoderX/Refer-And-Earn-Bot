b1bf6cfca69a4252c69e7eb7e87111c04de1a639
Bots.Business <git.sync@bots.business>
2025-04-15 16:15:08 +0000

    Bots.Business export for bot id: 1954582

---
+++ commands/_broadcast.js
@@ -0,0 +1,54 @@
+/*CMD
+  command: /broadcast
+  help: 
+  need_reply: false
+  auto_retry_time: 
+  folder: 
+
+  <<ANSWER
+
+  ANSWER
+
+  <<KEYBOARD
+
+  KEYBOARD
+  aliases: 
+  group: 
+CMD*/
+
+// get admin panel values
+var values = AdminPanel.getPanelValues("PANEL");
+
+// check if the user is an admin
+var admins = values.ADMINS;
+if (!admins || !admins.split(",").map(e => e.trim()).includes(user.telegramid.toString())) {
+  Api.sendMessage({
+    text: "🚫 You are not authorized to do this.\n\n Only admins can do this and you are not an admin"
+  });
+  return;
+}
+
+if (!chat || chat.chat_type !== "private") {
+    return;
+  }
+  
+  var message_id = request?.reply_to_message?.message_id;
+  var chat_id = request?.reply_to_message?.chat?.id;
+  
+  if (!message_id) {
+    Api.sendMessage({
+      text: "Please reply to a message to broadcast it.",
+      reply_to_message_id: request.message_id,
+    });
+    return;
+  }
+  
+  Bot.runAll({
+    command: "/notification",
+    for_chats: "private-chats",
+    on_create: "/broadcast_task",
+    options: {
+      chat_id: chat_id,
+      message_id: message_id,
+    }
+  })
--- commands/_broadcast_task.js
+++ commands/_broadcast_task.js
@@ -24,6 +24,6 @@ if (!task) {return}
 Bot.setProp("broadcast_task_id", task.id);
 
 Api.sendMessage({
-    text: `Broadcast task created successfully!\n\n<b>Task ID:</b> ${task.id}\n<b>Current Position:</b> ${task.cur_position}\n<b>Status Code:</b> ${task.status_code}\n\n<blockquote>You can check the status of the task using the command /broadcast_status</>`,
+    text: `Broadcast task created successfully!\n\n<b>Task ID:</b> ${task.id}\n<b>Current Position:</b> ${task.cur_position}\n<b>Status Code:</b> ${task.status_code}\n\n<blockquote>You can check the status of the task using the command /status</>`,
     parse_mode: "HTML"
 });
